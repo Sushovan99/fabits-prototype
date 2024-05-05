9a99f25d21e634428b4cce3ba2f73e12895ad09a
Sushovan Biswas <biswassushovan99@gmail.com>
2024-05-05 19:49:21 +0530

    Remove router devtool from production.

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,11 +1,13 @@
 import { createRootRoute, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 
+const isDev = import.meta.env.DEV;
+
 export const Route = createRootRoute({
     component: () => (
         <>
             <Outlet />
-            <TanStackRouterDevtools />
+            {isDev && <TanStackRouterDevtools />}
         </>
     ),
 });
